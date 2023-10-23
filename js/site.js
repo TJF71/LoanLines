a2e906f2960d992b68dff5fe6a0242b8eefc2374
TJF71 <138714842+TJF71@users.noreply.github.com>
2023-10-23 12:58:56 -0400

    initial git commit

--- js/site.js
+++ js/site.js
@@ -1,14 +1,7 @@
-function displayMessage() {
-// get the text out of the input and assign it to a variable
-    let msg = document.getElementById('message').value;
 
 
- Swal.fire(
-    {
-        backdrop: false,
-        title: 'APP NAME',
-        text: msg,  //replace this with that variable
-    }
- );
 
+function getLoanAmount() {
+    let loanamount = document.getElementsById('loanAmount')
 }
+
