11b09a58671dca4a9276b727f457279d6cc8fefd
TJF71 <138714842+TJF71@users.noreply.github.com>
2023-10-23 14:46:21 -0400

    buttone update

--- js/site.js
+++ js/site.js
@@ -1,7 +1,35 @@
-
-
-
 function getLoanAmount() {
-    let loanamount = document.getElementsById('loanAmount')
+    let loanamount = document.getElementById('loanAmount').value;
+
+    return loanamount;
 }
 
+function getInterest(){;
+
+    let interest = document.getElementById('interestRate').value;
+
+    return interest;
+}
+
+
+function getTerm(){
+
+    let totalPrincipal = document.getElementById('loanTerm').value;
+
+
+}
+
+
+function calcutatePayments(){
+
+
+}
+
+function displayPayments() {
+    
+    let principal = getLoanAmount();
+
+    document.getElementById('total-principal').textContent = `${principal}`;
+
+
+}
