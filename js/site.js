6bc242b25ff68090ea32a525147180567ce91ffa
TJF71 <138714842+TJF71@users.noreply.github.com>
2023-10-23 17:37:48 -0400

    update with correct monthly payment

--- js/site.js
+++ js/site.js
@@ -1,26 +1,25 @@
-function getLoanAmount() {
+function getValues() {
     let loanamount = document.getElementById('loanAmount').value;
-
-    return loanamount;
-}
-
-function getInterest(){;
-
     let interest = document.getElementById('interestRate').value;
+    let term = document.getElementById('loanTerm').value;
 
-    return interest;
-}
-
-
-function getTerm(){
-
-    let totalPrincipal = document.getElementById('loanTerm').value;
-
+    calcutatePayments(loanamount, interest, term)
 
 }
 
 
-function calcutatePayments(){
+
+
+function calcutatePayments(principal, rate, term){
+    let loan = principal;
+    let percent = rate;
+    let time = term;
+    let negTime = -time;
+    let denom = 1+ (rate/1200);
+
+    let monthlyPayment = (loan * (percent/1200)) /( 1-(denom**negTime));
+    console.log ("the monthly payment is ", monthlyPayment);
+
 
 
 }
