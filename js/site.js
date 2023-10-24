82caf4f273c2f28ce38ae416baeb2ec7ed38e4b3
TJF71 <138714842+TJF71@users.noreply.github.com>
2023-10-24 09:17:50 -0400

    updates

--- js/site.js
+++ js/site.js
@@ -8,43 +8,69 @@ function getValues() {
 }
 
 
-function calcutatePayments(principal, rate, term){
+function calcutatePayments(principal, rate, term) {
     let loan = principal;
     let percent = rate;
     let time = term;
     let negTime = -time;
-    let denom = 1 + (rate/1200);
+    let denom = 1 + (rate / 1200);
 
-    let monthlyPayment = (loan * (percent/1200)) /( 1-(denom**negTime));
+    let monthlyPayment = (loan * (percent / 1200)) / (1 - (denom ** negTime));
 
     let remainingBalance = loan;
-    let interestPayment = (remainingBalance * percent)/1200;
-    let principalPayment = monthlyPayment-interestPayment;
-    
+    let interestPayment = (remainingBalance * percent) / 1200;
+    let principalPayment = monthlyPayment - interestPayment;
 
-    displayPayments(loan, monthlyPayment, time,)
+
+    displayPayments(loan, monthlyPayment, time, percent)
 
 
 }
 
-function displayPayments(loan, monthlyPayment, time) {
-    
+function displayPayments(loan, monthlyPayment, time, percent) {
+
     const paymentTable = document.getElementById('payment-table');
 
     // clear the table
     paymentTable.innerHTML = '';
 
     let principal = loan;
-    let mPayment = monthlyPayment.toLocaleString('en-US', {style: 'currency',  currency: 'USD',});
+    let interest = percent;
+    let monthPayment = monthlyPayment
     let months = time;
+    let interestPayment = 0;
+    let principalPayment = 0;
+    let remainingBalance = 0
+    // let mPayment = monthlyPayment.toLocaleString('en-US', {style: 'currency',  currency: 'USD',});
+
+
 
     document.getElementById('total-principal').textContent = `Total Princiapl is: ${principal}`;
-    document.getElementById('monthly-payments').textContent  = `${mPayment}`;
+    document.getElementById('monthly-payments').textContent = `${monthPayment}`;
 
-    for (let i = 0; i < months; i++){
-        let month = month[i];
-        
-        
+    for (let i = 0; i < months; i++) {
+      
+
+        interest = principal * (interest / 1200);
+
+        principalPayment = monthPayment - interest;
+
+
+        remainingBalance = principal - principalPayment 
+
+        principal = remainingBalance;
+
+
+
+        console.log(interest, principalPayment, remainingBalance, principal)
+
+
+
+        // let eventRow = document.createElement('tr');
+        // let  period = document.createElement('td'); 
+        // eventRow.appendChild(period);
+
+
     }
 
 }
