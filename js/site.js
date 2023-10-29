de54619bf2598b8886eaab3433ca112b1c47a26c
TJF71 <138714842+TJF71@users.noreply.github.com>
2023-10-29 11:35:26 -0400

    updates

--- js/site.js
+++ js/site.js
@@ -1,86 +1,60 @@
+// get the trems of the loan to calculagte and validate them
 function getValues() {
-    let loanamount = document.getElementById('loanAmount').value;
-    let interest = document.getElementById('interestRate').value;
+    let principal  = document.getElementById('loanAmount').value;
     let term = document.getElementById('loanTerm').value;
+    let interest = document.getElementById('interestRate').value;
 
-    calcutatePayments(loanamount, interest, term)
+    principal = parseInt(loanAmount);
+    term = parseInt(term);
+    interest = parseFloat(interest);
 
-}
-
-
-function calcutatePayments(principal, rate, term) {
-    let loan = principal;
-    let percent = rate;
-    let time = term;
-    let negTime = -time;
-    let denom = 1 + (rate / 1200);
-
-    let monthlyPayment = (loan * (percent / 1200)) / (1 - (denom ** negTime));
-
-    let remainingBalance = loan;
-    let interestPayment = (remainingBalance * percent) / 1200;
-    let principalPayment = monthlyPayment - interestPayment;
-
-
-    displayPayments(loan, monthlyPayment, time, percent)
-
-
-}
-
-function displayPayments(loan, monthlyPayment, time, percent) {
-
-    const paymentTable = document.getElementById('payment-table');
-
-    // clear the table
-    paymentTable.innerHTML = '';
-
-    let principal = loan;
-    let interest = percent;
-    let monthPayment = monthlyPayment
-    let months = time;
-    let interestPayment = 0;
-    let principalPayment = 0;
-    let remainingBalance = 0
-    // let mPayment = monthlyPayment.toLocaleString('en-US', {style: 'currency',  currency: 'USD',});
-
-
-
-    document.getElementById('total-principal').textContent = `Total Princiapl is: ${principal}`;
-    document.getElementById('monthly-payments').textContent = `${monthPayment}`;
-
-    for (let i = 0; i < months; i++) {
-      
-        console.log ("the start principal is:", principal);
-        console.log ("the start interest is: ", interest);
-        // interest = principal * interest / 1200;
-        interestPayment = principal * interest / 1200;
-        console.log ("The interest is still", interest);
-        console.log ("The interest payment is", interestPayment);
-
-  
-
-        principalPayment = monthPayment - interest;
-        console.log ("the principal payment is", principalPayment)
-        console.log ("The total paymnt is ", principalPayment + interestPayment);
-
-
-        remainingBalance = principal - principalPayment;
-        console.log("The remaining balance is ", remainingBalance); 
-
-        principal = remainingBalance;
-        // console.log(principal);
-
-
-
-
-
-
-
-        // let eventRow = document.createElement('tr');
-        // let  period = document.createElement('td'); 
-        // eventRow.appendChild(period);
-
-
+    if (isNaN(principal) || isNaN(term) || isNaN(interest) || principal <= 0 || term <= 0 || interest <= 0){
+        Swal.fire({
+            icon:'error',
+            backdrop: false,
+            title: 'Oops',
+            text: 'Please enter valid numbers for your loan'
+        })
+    } else {
+        let totals = calculateTotals(principal, term, rate);
+        displayTotals(totals)
     }
 
 }
+
+// calculate the totals for the loan
+function calculateTotals (principal, term, rate){
+
+    let monthlyPayment = (principal * (rate/1200)) / (1 - Math.pow(1 + rate/1200, -term));
+    let totalCost = monthlyPayment * term;
+    let totalInterest = totalCost = principall;
+
+    // return an object with all the totals
+    let loanTotals = {
+        monthlyPayment: monthlyPayment,
+        totalCost: totalCost,
+        totalInterest: totalInterest, 
+        totalPrincipal: principal       
+    }
+
+    return loanTotals;
+}
+
+
+// calculate each month of the loan
+function calculatePayments(paymentsArr){
+
+}
+
+// display the totals for the loan
+function displayTotals(loan){
+
+}
+
+function displayPayments(paymentsArr){
+
+}
+
+
+
+// display the month in a table
